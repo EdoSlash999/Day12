34b33b20b34211ec9f3c6b3cb22fbf9b6a393f98
EdoSlash999 <edo_tz_@hotmail.com>
2021-03-20 06:48:56 +0100

    rijesen zadatak

---
+++ ex03/src/index.js
@@ -0,0 +1,22 @@
+// Only change code below this line
+function rangeOfNumbers(startN, endN) {
+	if (startN > endN) {
+		return "The starting number will always be less than or equal to the ending number";
+	}
+	if (startN - endN === 0) {
+		return [endN];
+	} 
+    else 
+    {
+		const numbers = rangeOfNumbers(startN + 1, endN);
+		numbers.unshift(startN);
+		return numbers;
+	}
+}
+
+// Only change code above this line
+console.log(rangeOfNumbers(1, 7));
+console.log(rangeOfNumbers(3, 10));
+console.log(rangeOfNumbers(5, 5));
+console.log(rangeOfNumbers(7, 5));
+module.exports = rangeOfNumbers;
